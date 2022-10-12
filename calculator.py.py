b94a3977960e0183f38a9671e5d35f6a0d3fa116
dheeraj2488 <115392626+dheeraj2488@users.noreply.github.com>
2022-10-12 23:27:27 +0530

    Add files via upload

---
+++ calculator.py.py
@@ -0,0 +1,10 @@
+a= int(input("enter any number1"))
+b= int(input("enter any number2"))
+#by int we means the integer otherise it is a string
+print("addition is", a+b)
+print("subtraction is", a-b)
+print("multiplicatiin is", a*b)
+print("division is", a/b)
+print("exponentiaion is", a**b)
+print("floor division is", a//b)
+print("modulus", a%b)
